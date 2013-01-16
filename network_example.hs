d339abd7e949b7d584a46f8807b31723cef12620
Jason Hickner <jhickner@gmail.com>
2013-01-16 14:35:04 -0800

    updated examples

--- network_example.hs
+++ network_example.hs
@@ -3,22 +3,29 @@ module Main where
 import Data.Modbus
 import Network
 import Network.Socket
+import Control.Exception
 
 main :: IO ()
-main = do
-    sock <- openSocket "hostname" 502
-    resp <- modFunction (networkTransport sock) timeout req
-    print resp
-    sClose sock
-  where
-    timeout = 50000
-    req = AduRequest 1 $ ReadHoldingRegisters 120 8
+main = 
+  let host = "75.149.170.14"
+      port = 502
+      timeout = 500000
+      serverId = 1
+      address = 120
+      registerCount = 8
+      req = AduRequest serverId $ ReadHoldingRegisters address registerCount
+  in
+    withSocket host port $ \s -> do
+        resp <- modFunction (networkTransport s) timeout req
+        print resp
 
--- | open a socket to the given host and port
-openSocket :: HostName -> Int -> IO Socket
-openSocket host port = do
-  addrinfos <- getAddrInfo Nothing (Just host) (Just $ show port)
-  let serveraddr = head addrinfos
-  sock <- socket (addrFamily serveraddr) Stream defaultProtocol
-  connect sock (addrAddress serveraddr)
-  return sock
+-- | open a socket and run the specified action
+withSocket :: HostName -> Int -> (Socket -> IO ()) -> IO ()
+withSocket host port = 
+    bracket
+      (do addrinfos <- getAddrInfo Nothing (Just host) (Just $ show port)
+          let serveraddr = head addrinfos
+          sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+          connect sock (addrAddress serveraddr)
+          return sock)
+      sClose
--- serial_example.hs
+++ serial_example.hs
@@ -2,30 +2,26 @@ module Main where
 
 import Data.Modbus
 import qualified System.Hardware.Serialport as SP
-import Control.Concurrent
 
 main :: IO ()
 main = 
   let port = "/dev/ttyUSB0" 
-      ss = SP.SerialPortSettings {SP.commSpeed = SP.CS9600,
-            SP.bitsPerWord = 8,
-            SP.stopb = SP.One,
-            SP.parity = SP.Even,
-            SP.flowControl = SP.NoFlowControl,
-            SP.timeout = 1} in
-  do
+      ss = SP.SerialPortSettings 
+        { SP.commSpeed = SP.CS9600
+        , SP.bitsPerWord = 8
+        , SP.stopb = SP.One
+        , SP.parity = SP.Even
+        , SP.flowControl = SP.NoFlowControl
+        , SP.timeout = 1
+        } 
+      timeout = 100000
+      retries = 3
+      serverId = 3
+      address = 0x1000
+      registerCount = 2
+      req = AduRequest serverId $ ReadHoldingRegisters address registerCount
+  in do
     s <- SP.openSerial port ss
-    loop s
+    resp <- retryModFunction (serialTransport s) timeout retries req
+    print resp
     SP.closeSerial s
-  where
-    loop s = 
-      let serverId = 3
-          address  = 0x1000
-          registerCount = 2
-          timeoutUs = 100000
-          retries = 3 
-          modbusRequest = AduRequest serverId $ ReadHoldingRegisters address registerCount in do
-        resp <- retryModFunction (serialTransport s) timeoutUs retries modbusRequest
-        print $ show resp
-        threadDelay 750000
-        loop s
